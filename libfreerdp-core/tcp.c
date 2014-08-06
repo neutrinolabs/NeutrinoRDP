b3e03d55f3555026ecb73b995c20ee52fa143e4f
Jay Sorg <jay.sorg@gmail.com>
2014-08-06 16:02:51 -0700

    turn off debug log

--- libfreerdp-core/tcp.c
+++ libfreerdp-core/tcp.c
@@ -53,7 +53,7 @@
 
 #include "tcp.h"
 
-#define LLOG_LEVEL 11
+#define LLOG_LEVEL 1
 #define LLOGLN(_level, _args) \
   do { if (_level < LLOG_LEVEL) { printf _args ; printf("\n"); } } while (0)
 #define LHEXDUMP(_level, _args) \
