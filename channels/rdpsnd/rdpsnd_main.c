cb41e5005f97348b084838975cb529684e264371
Jay Sorg <jay.sorg@gmail.com>
2015-07-19 11:28:58 -0700

    rdpsnd fix for pulse assert

--- channels/rdpsnd/rdpsnd_main.c
+++ channels/rdpsnd/rdpsnd_main.c
@@ -433,9 +433,8 @@ static void rdpsnd_process_message_close(rdpsndPlugin* rdpsnd)
 {
 	DEBUG_SVC("server closes.");
 	if (rdpsnd->device)
-		IFCALL(rdpsnd->device->Start, rdpsnd->device);
-	rdpsnd->close_timestamp = get_mstime() + 2000;
-	rdpsnd->plugin.interval_ms = 10;
+		IFCALL(rdpsnd->device->Close, rdpsnd->device);
+	rdpsnd->is_open = false;
 }
 
 static void rdpsnd_process_message_setvolume(rdpsndPlugin* rdpsnd, STREAM* data_in)
